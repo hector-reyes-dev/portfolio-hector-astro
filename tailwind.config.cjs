abdf72f53b1e500675eb3050291525dcb2791bf4
Hector <hectordevx@gmail.com>
2023-05-26 21:27:22 -0500

    feat(assets): Tailwind Theme Config Colors and Assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fbfbfb",
+        "light-gray": "#e0e0e5",
+        gray: "#9796a3",
+        "dark-gray": "#484753",
+        black: "#1e1e29",
+        red: "#ff0844",
+        "red-light": "#ff537d",
+        orange: "#ff8c69",
+        "orange-light": "#ffb199",
+        purple: "#6b42df",
+        blue: "#6bb5f9",
+      },
+    },
+  },
+  plugins: [],
+};
