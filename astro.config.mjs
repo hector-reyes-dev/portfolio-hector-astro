b3d57f29789802803abefed643f11ed39685a18f
Hector <hectordevx@gmail.com>
2024-03-07 14:37:18 -0600

    fix(analytics): Partytown and title added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://hectorcreative.dev/",
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      config: { forward: ["dataLayer.push"] },
+    }),
+  ],
 });
