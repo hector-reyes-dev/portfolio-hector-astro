cd4cb637d251a085bd7d2db9453e7ff7c6b55f09
Héctor Reyes <=>
2025-06-21 18:35:01 -0600

    fix(deploy): Netlify adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import icon from "astro-icon";
-
+import netlify from "@astrojs/netlify/server";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hectorcreative.dev/",
+  output: "server",
+  adapter: netlify(),
   integrations: [
     tailwind(),
     react(),
